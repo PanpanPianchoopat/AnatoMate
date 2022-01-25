54a75d5fe768d83eb6d77be3fb6a97a48b53c882
Ploypapas Pianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2022-01-25 16:44:48 +0700

    Revert "Home page (#2)"
    This reverts commit 7998fba459ace87d6d7346228c07e79fe0be39c4.

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledButton } from "./styled";
-
-const Button = ({ children, ...props }) => {
-  return <StyledButton {...props}>{children}</StyledButton>;
-};
-
-export default Button;
