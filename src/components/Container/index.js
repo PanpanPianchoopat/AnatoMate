208afb8b965931492c7e12b35bfcea79210a0e95
Chatchapon <leawgamer1@hotmail.com>
2022-01-25 16:14:56 +0700

    Remake

--- src/components/Container/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { StyledContainer } from "./styled";
-const Container = ({ children }) => {
-  return <StyledContainer>{children}</StyledContainer>;
-};
-
-export default Container;
