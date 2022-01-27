15d4e0cf1755fcfb66c93cf0f9945d358c62aadf
PanpanPianchoopat <pploypapass@gmail.com>
2022-01-27 14:30:37 +0700

    Add gridbg for login

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -15,33 +15,6 @@ const Register = () => {
           Already have an account? <a href="/login">Log in</a>
         </small>
         <br />
-        {/* <Form>
-          <Form.Item>
-            <Input
-              placeholder="Email"
-              style={{ border: "2px solid black", height: "40px" }}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input.Password
-              placeholder="Password"
-              style={{ border: "2px solid black", height: "40px" }}
-            />
-            <small>
-              Min 8 characters include number, special character, uppercase and
-              lowercase letters
-            </small>
-          </Form.Item>
-          <Form.Item>
-            <Input.Password
-              placeholder="Confirm Password"
-              style={{ border: "2px solid black", height: "40px" }}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button style={{ width: "100%", marginTop: "20px" }}>Next</Button>
-          </Form.Item>
-        </Form> */}
         <Form1 />
       </FormContainer>
     </GridBg>
